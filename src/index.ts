e9b5591ffdc8f85f861d82e59e4b646d75c46dbd
Egge <egge24@protonmail.com>
2024-01-16 06:49:38 +0100

    handeled cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyparser from "body-parser";
+import cors from "cors";
 import { CashuMint, CashuWallet } from "@cashu/cashu-ts";
 import routes from "./routes";
 
@@ -7,6 +8,7 @@ export const wallet = new CashuWallet(new CashuMint(process.env.MINTURL!));
 const app = express();
 
 app.use(bodyparser.json());
+app.use(cors());
 
 app.use(routes);
 
