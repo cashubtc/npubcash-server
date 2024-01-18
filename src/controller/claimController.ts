a64c87bb3eb3a9ee252808dfd81617538cc5bc18
Egge <egge24@protonmail.com>
2024-01-18 16:57:27 +0100

    added balance and filter

--- src/controller/claimController.ts
+++ src/controller/claimController.ts
@@ -2,9 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { verifyAuth } from "../utils/auth";
 import { User } from "../models/user";
 import { Claim } from "../models/claim";
-import { TokenEntry, getDecodedToken } from "@cashu/cashu-ts";
+import {
+  CashuMint,
+  CashuWallet,
+  Proof,
+  TokenEntry,
+  getDecodedToken,
+  getEncodedToken,
+} from "@cashu/cashu-ts";
 
-export async function claimController(
+export async function balanceController(
   req: Request,
   res: Response,
   next: NextFunction,
@@ -16,7 +23,7 @@ export async function claimController(
   }
   const isAuth = await verifyAuth(
     authHeader,
-    "https://cashu.my2sats.space/claim",
+    "https://cashu.my2sats.space/balance",
     "GET",
   );
   if (!isAuth.authorized) {
@@ -34,20 +41,71 @@ export async function claimController(
     allClaims = npubClaims;
   }
   const decodedToken = allClaims.map((claim) => getDecodedToken(claim.token));
-  const entries = [] as TokenEntry[];
+  const proofs: Proof[] = [];
   decodedToken.forEach((token) => {
     token.token.forEach((entry) => {
-      entries.push(entry);
+      entry.proofs.forEach((p) => proofs.push(p));
     });
   });
-  const balance = entries.reduce((a, c) => {
-    let val = 0;
-    c.proofs.forEach((proof) => {
-      val = val + proof.amount;
-    });
-    return a + val;
-  }, 0);
+  const payload = {
+    proofs: proofs.map((p) => ({ secret: p.secret })),
+  };
+  const { spendable } = await new CashuMint(process.env.MINTURL!).check(
+    payload,
+  );
+  const spendableProofs = proofs.filter((_, i) => spendable[i]);
+  const balance = spendableProofs.reduce((a, c) => a + c.amount, 0);
   return res.json({ error: false, data: balance });
+}
+
+export async function claimGetController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
+    res.status(401);
+    return next(new Error("Missing Authorization Header"));
+  }
+  const isAuth = await verifyAuth(
+    authHeader,
+    "https://cashu.my2sats.space/claim",
+    "GET",
+  );
+  if (!isAuth.authorized) {
+    res.status(401);
+    return next(new Error("Invalid Authorization Header"));
+  }
+  const user = await User.getUserByPubkey(isAuth.data.pubkey);
+  let allClaims: Claim[];
+  if (user) {
+    const userClaims = await Claim.getUserClaims(user.name);
+    const npubClaims = await Claim.getUserClaims(isAuth.data.npub);
+    allClaims = [...userClaims, ...npubClaims];
+  } else {
+    allClaims = await Claim.getUserClaims(isAuth.data.npub);
+  }
+  const decodedToken = allClaims.map(
+    (claim) => getDecodedToken(claim.token).token,
+  );
+  const proofs: Proof[] = [];
+  decodedToken.forEach((token) => {
+    token.forEach((e) => e.proofs.forEach((proof) => proofs.push(proof)));
+  });
+  const payload = { proofs: proofs.map((p) => ({ secret: p.secret })) };
+  const { spendable } = await new CashuMint(process.env.MINTURL!).check(
+    payload,
+  );
+  const spendableProofs = proofs.filter((_, i) => spendable[i]);
+  const token = getEncodedToken({
+    memo: "",
+    token: [{ mint: process.env.MINTURL!, proofs: spendableProofs }],
+  });
+  if (spendableProofs.length === 0) {
+    return res.json({ error: true, message: "No proofs to claim" });
+  }
+  res.json({ error: false, data: { token: token } });
 }
 
 export async function claimPostController(
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { lnurlController } from "./controller/lnurlController";
 import { paidController } from "./controller/paidController";
-import { claimController } from "./controller/claimController";
+import {
+  balanceController,
+  claimController,
+  claimGetController,
+} from "./controller/claimController";
 
 const routes = Router();
 
 routes.get("/.well-known/lnurlp/:user", lnurlController);
 routes.post("/paid", paidController);
-routes.get("/claim", claimController);
+routes.get("/claim", claimGetController);
+routes.get("/balance", balanceController);
 
 export default routes;
