15b984be16b2a154a94ce803ccbdcfc0f309b3f8
Egge <egge24@protonmail.com>
2024-02-14 16:35:03 +0100

    replaced env var with host header

--- src/controller/claimController.ts
+++ src/controller/claimController.ts
@@ -1,27 +1,9 @@
-import { NextFunction, Request, Response } from "express";
-import { verifyAuth } from "../utils/auth";
+import { Request, Response } from "express";
 import { CashuMint, Proof, getEncodedToken } from "@cashu/cashu-ts";
 import { Claim, User } from "../models";
 
-export async function balanceController(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  const authHeader = req.header("Authorization");
-  if (!authHeader) {
-    res.status(401);
-    return next(new Error("Unauthorized"));
-  }
-  const isAuth = await verifyAuth(
-    authHeader,
-    `${process.env.HOSTNAME}/api/v1/balance`,
-    "GET",
-  );
-  if (!isAuth.authorized) {
-    res.status(401);
-    return next(new Error("Unauthorized"));
-  }
+export async function balanceController(req: Request, res: Response) {
+  const isAuth = req.authData!;
   const user = await User.getUserByPubkey(isAuth.data.pubkey);
   let allClaims: Claim[];
   if (user) {
@@ -48,41 +30,20 @@ export async function balanceController(
       unspendableClaims.push(allClaims[i]);
     }
   }
-  Claim.updateClaimsStatus(
-    unspendableClaims.map((claim) => claim.id),
-    "spent",
-  );
+  Claim.updateClaimsStatus(unspendableClaims.map((claim) => claim.id));
   const balance = spendableProofs.reduce((a, c) => a + c.amount, 0);
   return res.json({ error: false, data: balance });
 }
 
-export async function claimGetController(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  const authHeader = req.header("Authorization");
-  if (!authHeader) {
-    res.status(401);
-    return next(new Error("Missing Authorization Header"));
-  }
-  const isAuth = await verifyAuth(
-    authHeader,
-    `${process.env.HOSTNAME}/api/v1/claim`,
-    "GET",
-  );
-  if (!isAuth.authorized) {
-    res.status(401);
-    return next(new Error("Invalid Authorization Header"));
-  }
-  const user = await User.getUserByPubkey(isAuth.data.pubkey);
+export async function claimGetController(req: Request, res: Response) {
+  const user = await User.getUserByPubkey(req.authData!.data.pubkey);
   let allClaims: Claim[];
   if (user) {
     const userClaims = await Claim.getUserReadyClaims(user.name);
-    const npubClaims = await Claim.getUserReadyClaims(isAuth.data.npub);
+    const npubClaims = await Claim.getUserReadyClaims(req.authData!.data.npub);
     allClaims = [...userClaims, ...npubClaims];
   } else {
-    allClaims = await Claim.getUserReadyClaims(isAuth.data.npub);
+    allClaims = await Claim.getUserReadyClaims(req.authData!.data.npub);
   }
   const proofs = allClaims.map((claim) => claim.proof);
   const payload = { proofs: proofs.map((p) => ({ secret: p.secret })) };
