8ae73915a6e00e8d057058815af132daccaf4683
Egge <egge24@protonmail.com>
2024-02-16 07:40:20 +0100

    host header test

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,17 +4,19 @@ import { verifyAuth } from "../utils/auth";
 export function isAuthMiddleware(path: string, method: string) {
   async function isAuth(req: Request, res: Response, next: NextFunction) {
     const hostname = req.header("host");
-    const protocol = req.protocol;
+    const protocol = req.header("X-Forwarded-Proto");
     if (!hostname) {
       res.status(400);
       return next(new Error("Missing host header"));
     }
+    console.log(hostname);
     const url = protocol + "://" + hostname + path;
     const authHeader = req.header("Authorization");
     if (!authHeader) {
       res.status(401);
       return next(new Error("Missing Authorization Header"));
     }
+    console.log(url);
     const isAuth = await verifyAuth(authHeader, url, method);
     if (!isAuth.authorized) {
       res.status(401);
