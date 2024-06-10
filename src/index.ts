7c5018fab4678fb0706490fa7e6102595c4bcbc4
Egge <egge@getcurrent.io>
2024-06-10 06:38:49 +0200

    automatic callback setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { useWebSocketImplementation } from "nostr-tools";
 import app from "./app";
 import { setupDatabase } from "./utils/database";
+import { setupCallbacks } from "./utils/blink";
 
 useWebSocketImplementation(require("ws"));
 
@@ -10,6 +11,14 @@ async function startServer() {
   } catch (e) {
     console.warn("Database Migrations failed!!");
     console.log(e);
+    process.exit(1);
+  }
+  try {
+    await setupCallbacks();
+  } catch (e) {
+    console.warn("Failed to setup callbacks...");
+    console.log(e);
+    process.exit(1);
   }
   console.log("starting server...");
   app.listen(process.env.PORT || 8000);
