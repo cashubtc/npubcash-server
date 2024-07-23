81773be633d55f6980434baf9f7e6baa235c0f35
Egge <egge@getcurrent.io>
2024-07-23 12:54:18 +0200

    added withdrawal store

--- src/models/withdrawal.ts
+++ src/models/withdrawal.ts
@@ -1,3 +1,4 @@
+import { Pool } from "pg";
 import { createSanitizedValueString, getDbClient } from "../utils/database";
 import { Claim } from "./claim";
 
@@ -25,6 +26,52 @@ export class Withdrawal {
         this.pubkey,
         ids,
       ]);
+      const list = createSanitizedValueString(ids.length);
+      const claimUpdateQuery = `UPDATE l_claims_3 SET status = 'spent' WHERE id in ${list}`;
+      await client.query(claimUpdateQuery, ids);
+    } catch (e) {
+      console.warn("Failed to create withdrawl... rolling back");
+      client.query("ROLLBACK");
+      throw e;
+    } finally {
+      client.release();
+    }
+  }
+}
+
+export class WithdrawalStore {
+  private pool: Pool;
+  static instance: WithdrawalStore;
+
+  constructor(pool: Pool) {
+    this.pool = pool;
+  }
+
+  static getInstance(pool: Pool) {
+    if (this.instance) {
+      return this.instance;
+    } else {
+      if (pool) {
+        this.instance = new WithdrawalStore(pool);
+      } else {
+        throw new Error("Not instantiated yet...");
+      }
+    }
+  }
+
+  async saveWithdrawal(claims: Claim[], pubkey: string) {
+    const client = await this.pool.connect();
+    let amount = 0;
+    let ids: number[] = [];
+    for (let i = 0; i < claims.length; i++) {
+      amount += claims[i].proof.amount;
+      ids.push(claims[i].id);
+    }
+    try {
+      await client.query("BEGIN");
+      const ids = claims.map((c) => c.id);
+      const withDrawalInsertQuery = `INSERT INTO l_withdrawals (amount, pubkey, claimIds) VALUES ($1, $2, $3)`;
+      await client.query(withDrawalInsertQuery, [amount, pubkey, ids]);
       const list = createSanitizedValueString(ids.length);
       const claimUpdateQuery = `UPDATE l_claims_3 SET status = 'spent' WHERE id in ${list}`;
       await client.query(claimUpdateQuery, ids);
