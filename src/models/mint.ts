14f6a9669679a0df53016070539748a5716ca481
Egge <egge@getcurrent.io>
2025-04-01 06:37:21 +0200

    refactored mint model

--- src/models/mint.ts
+++ src/models/mint.ts
@@ -1,53 +1,67 @@
 import { queryWrapper } from "@/utils/database";
 
-export class MintQuote {
-  private constructor(
-    public id: number,
-    public created_at: number,
-    public mint_url: string,
-    public payment_request: string,
-    public quote_id: string,
-    public expires_at: Date,
-    public amount: number,
-    public pubkey: string,
-    public state: "PAID" | "UNPAID" | "INFLIGHT" | "ISSUED" | "EXPIRED",
-  ) {}
+interface MintQuoteConfig {
+  id: number;
+  created_at: number;
+  mint_url: string;
+  payment_request: string;
+  quote_id: string;
+  expires_at: Date;
+  amount: number;
+  pubkey: string;
+  state: "PAID" | "UNPAID" | "INFLIGHT" | "ISSUED" | "EXPIRED";
+}
+
+export class MintQuote implements MintQuoteConfig {
+  id: number;
+  created_at: number;
+  mint_url: string;
+  payment_request: string;
+  quote_id: string;
+  expires_at: Date;
+  amount: number;
+  pubkey: string;
+  state: "PAID" | "UNPAID" | "INFLIGHT" | "ISSUED" | "EXPIRED";
+  private constructor(config: MintQuoteConfig) {
+    this.id = config.id;
+    this.created_at = config.created_at;
+    this.mint_url = config.mint_url;
+    this.payment_request = config.payment_request;
+    this.quote_id = config.quote_id;
+    this.expires_at = config.expires_at;
+    this.amount = config.amount;
+    this.pubkey = config.pubkey;
+    this.state = config.state;
+  }
 
   static async createNewMintQuoteInDb(
-    quote_id: string,
-    expires_at: Date,
-    payment_request: string,
-    mint_url: string,
-    amount: number,
-    pubkey: string,
+    config: Omit<MintQuoteConfig, "id" | "created_at" | "state">,
   ) {
-    const res = await queryWrapper<MintQuote>(
+    const res = await queryWrapper<MintQuoteConfig>(
       `INSERT INTO mint_quotes (mint_url, payment_request, quote_id, expires_at, amount, pubkey, state) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, created_at`,
       [
-        mint_url,
-        payment_request,
-        quote_id,
-        expires_at,
-        amount,
-        pubkey,
+        config.mint_url,
+        config.payment_request,
+        config.quote_id,
+        config.expires_at,
+        config.amount,
+        config.pubkey,
         "UNPAID",
       ],
     );
     if (res.rowCount === 0) {
       throw new Error("Failed to create new mint quote");
     }
-    const row = res.rows[0];
-    return new MintQuote(
-      row.id,
-      row.created_at,
-      row.mint_url,
-      row.payment_request,
-      row.quote_id,
-      row.expires_at,
-      row.amount,
-      row.pubkey,
-      row.state,
-    );
+    return new MintQuote(res.rows[0]);
+  }
+
+  async setStateAndUpdateDb(newState: MintQuote["state"]) {
+    const query = `UPDATE mint_quotes SET state = $1 WHERE id = $2`;
+    const res = await queryWrapper(query, [newState, this.id]);
+    if (res.rowCount === 0) {
+      throw new Error("Failed to update state");
+    }
+    this.state = newState;
   }
 
   static async getToBeExpiredMintQuotes() {
@@ -55,20 +69,7 @@ export class MintQuote {
       `SELECT * FROM mint_quotes WHERE expires_at <= NOW() AND state = "UNPAID"`,
       [],
     );
-    return res.rows.map(
-      (q) =>
-        new MintQuote(
-          q.id,
-          q.created_at,
-          q.mint_url,
-          q.payment_request,
-          q.quote_id,
-          q.expires_at,
-          q.amount,
-          q.pubkey,
-          q.state,
-        ),
-    );
+    return res.rows.map((r) => new MintQuote(r));
   }
 
   static async getPaidMintAmount(pubkey: string) {
@@ -91,28 +92,26 @@ export class MintQuote {
     WHERE pubkey = $1 and (state = 'INFLIGHT' or state = 'PAID');
     `;
     const res = await queryWrapper<MintQuote>(query, [pubkey]);
-    return res.rows.map(
-      (r) =>
-        new MintQuote(
-          r.id,
-          r.created_at,
-          r.mint_url,
-          r.payment_request,
-          r.quote_id,
-          r.expires_at,
-          r.amount,
-          r.pubkey,
-          r.state,
-        ),
-    );
+    return res.rows.map((r) => new MintQuote(r));
   }
 
-  async setStateAndUpdateDb(newState: MintQuote["state"]) {
-    const query = `UPDATE mint_quotes SET state = $1 WHERE id = $2`;
-    const res = await queryWrapper(query, [newState, this.id]);
-    if (res.rowCount === 0) {
-      throw new Error("Failed to update state");
-    }
-    this.state = newState;
+  static async bulkUpdateState(state: MintQuote["state"], quotes: MintQuote[]) {
+    const idMap = new Map<number, MintQuote>();
+    quotes.forEach((q) => {
+      idMap.set(q.id, q);
+    });
+    const ids = [...idMap.keys()];
+    const valueClause = ids.map((_, i) => "$" + (i + 2)).join(",");
+    const query = `
+    UPDATE mint_quotes
+    SET state = $1
+    WHERE id in (${valueClause});`;
+    const queryRes = await queryWrapper<MintQuote>(query, [state, ...ids]);
+    queryRes.rows.forEach((r) => {
+      const q = idMap.get(r.id);
+      if (q) {
+        q.state = state;
+      }
+    });
   }
 }
