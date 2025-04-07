c9977f441b040911307266830a315076b38f9ef3
Egge <egge@getcurrent.io>
2025-04-07 15:03:48 +0200

    mintComm with env url

--- src/config.ts
+++ src/config.ts
@@ -14,16 +14,13 @@ export const wallet = new CashuWallet(new CashuMint(process.env.MINTURL!));
 export const lnProvider = new LightningHandler(new BlinkProvider());
 export const nostrPool = new SimplePool();
 
-export const mintComm = new MintCommunicator(
-  "https://mint.minibits.cash/Bitcoin",
-  {
-    initialPollingTimeout: { mint: 10000, melt: 10000, proof: 10000 },
-    backoffFunction: (r) => Math.min(5000 * Math.pow(2, r), 600000),
-    throttleCapacity: 10,
-    throttleTimeout: 3500,
-    logger: new Logger(),
-  },
-);
+export const mintComm = new MintCommunicator(process.env.MINTURL!, {
+  initialPollingTimeout: { mint: 10000, melt: 10000, proof: 10000 },
+  backoffFunction: (r) => Math.min(5000 * Math.pow(2, r), 600000),
+  throttleCapacity: 10,
+  throttleTimeout: 3500,
+  logger: new Logger(),
+});
 
 export let ZAP_PUBKEY: string;
 if (process.env.ZAP_SECRET_KEY) {
