da26423d0a77799af2bd7de8ac3f8cda2d83b428
Egge <egge@getcurrent.io>
2025-04-16 17:26:39 +0200

    server: removed unused function

--- packages/server/src/utils/general.ts
+++ packages/server/src/utils/general.ts
@@ -1,5 +1,3 @@
-import { Response } from "express";
-
 export function checkEnvVars(vars: string[]) {
   const missing: string[] = [];
   for (let i = 0; i < vars.length; i++) {
@@ -10,13 +8,4 @@ export function checkEnvVars(vars: string[]) {
   if (missing.length) {
     throw new Error(`Missing environment variables: ${missing.join(",")}`);
   }
-}
-
-export function respondWithError(
-  res: Response,
-  errorCode: number,
-  errorMessage: string,
-) {
-  res.status(errorCode);
-  res.json({ error: true, message: errorMessage });
 }
