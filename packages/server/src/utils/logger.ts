a37e9b65d2091dcd389a3a2b20fa1ab686bf9fd2
Egge <egge@getcurrent.io>
2025-05-06 05:42:44 +0200

    server: log level env

--- packages/server/src/utils/logger.ts
+++ packages/server/src/utils/logger.ts
@@ -2,7 +2,7 @@ import { Request } from "express";
 import winston from "winston";
 
 export const logger = winston.createLogger({
-  level: process.env.NODE_ENV !== "production" ? "debug" : "info",
+  level: process.env.LOG_LEVEL === "DEBUG" ? "debug" : "info",
   format: winston.format.json(),
   transports: [new winston.transports.Console()],
 });
