b187139ed6d978590a59f3bc6aaa8311ca3ac9cf
Egge <egge@getcurrent.io>
2025-07-15 14:33:55 +0200

    feat(sdk): Configure SDK for ESM and CommonJS module formats

--- packages/sdk/vite.config.ts
+++ packages/sdk/vite.config.ts
@@ -5,11 +5,16 @@ export default defineConfig({
     outDir: "./dist",
     lib: {
       entry: "./src/index.ts",
-      name: "npubcash-sdk",
-      fileName: "npc-sdk",
+      formats: ["es", "cjs"],
+      fileName: (format) => `npc-sdk.${format === "es" ? "mjs" : "cjs"}`,
     },
     rollupOptions: {
       external: ["nostr-tools/nip98"],
+      output: {
+        globals: {
+          "nostr-tools/nip98": "nostrToolsNip98",
+        },
+      },
     },
   },
 });
