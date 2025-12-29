b20abb896a4fef96a151a71fb7c985c1c7077c80
Egge <egge@my2sats.space>
2025-12-29 20:51:06 +0100

    clean(server): removed dead v1 code

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import v1Router from "./v1";
 import v2Router from "./v2";
 import { nip05Controller } from "@/controller/nip05Controller";
 import { lnurlController } from "@/controller/lnurlController";
@@ -8,7 +7,6 @@ const baseRouter = Router();
 
 baseRouter.get("/.well-known/lnurlp/:user", lnurlController);
 baseRouter.get("/.well-known/nostr.json", nip05Controller);
-baseRouter.use("/api/v1", v1Router);
 baseRouter.use("/api/v2", v2Router);
 
 export default baseRouter;
