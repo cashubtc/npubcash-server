da36279607aba7380b6a4aa739fb4cc3cd74f585
Egge <egge@my2sats.space>
2026-01-08 06:50:30 +0100

    build(sdk): improved build pipeline

--- packages/sdk/vite.config.ts
+++ packages/sdk/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
   build: {
     outDir: "./dist",
     lib: {
